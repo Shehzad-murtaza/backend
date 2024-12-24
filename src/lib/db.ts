0499b7a10847d61ef16f8c00eace4039bc32beb7
Shehzad Murtaza <mr.shehzad457@gmail.com>
2024-12-25 02:07:20 +0500

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,18 +1,25 @@
+// src/lib/db.ts
 import mongoose from 'mongoose';
 
 const connectDB = async (): Promise<void> => {
-  if (!process.env.MONGODB_URI) {
-    console.error('MONGODB_URI is not defined');
+  // MongoDB connection string
+  const mongoURI = 'mongodb+srv://ali:abc1234567@cluster0.xht8ahs.mongodb.net/zero-master';
+
+  // Check if the connection string is defined
+  if (!mongoURI) {
+    console.error('MongoDB URI is not defined');
     process.exit(1); // Exit if the URI is not defined
   }
 
+  // Check if already connected
   if (mongoose.connections[0].readyState) {
     console.log('Already connected to the database');
     return;
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI as string);
+    // Connect to MongoDB
+    await mongoose.connect(mongoURI);
     console.log('Database connected');
   } catch (error) {
     console.error('Database connection error: ', error);
